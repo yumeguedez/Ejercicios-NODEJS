e9511b9c1fd2c88665c4670f48807b972df048d5
yumeguedez <yuguedez.dwfsc16@bootcamp.ucamp.io>
2023-04-13 21:48:13 -0500

    ejercicios node js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const calculadora = require("./calculadora");
+// const calculadora = require("./calculadora");
 
-const a = 20;
-const b = 12;
+// const a = 20;
+// const b = 12;
 
-const resultadoSuma = calculadora.suma(a, b);
-const resultadoMultiplicacion = calculadora.multiplicacion(a, b);
+// const resultadoSuma = calculadora.suma(a, b);
+// const resultadoMultiplicacion = calculadora.multiplicacion(a, b);
+
+// console.log(`La Suma de ${a} y ${b} es: ${resultadoSuma}`);
+
+// console.log(`La Multiplicacion de ${a} y ${b} es: ${resultadoMultiplicacion}`);
+
+const { multiplicacion } = require("./calculadora");
+const division = require("./operaciones/division");
+const resta = require("./operaciones/resta");
+const suma = require("./operaciones/suma");
+
+const a = 10;
+const b = 5;
+
+const resultadoSuma = suma(a, b);
+const resultadoResta = resta(a, b);
+const resultadoMultiplicacion = multiplicacion(a, b);
+const resultadoDivision = division(a, b);
 
 console.log(`La Suma de ${a} y ${b} es: ${resultadoSuma}`);
-
+console.log(`La Resta de ${a} y ${b} es: ${resultadoResta}`);
 console.log(`La Multiplicacion de ${a} y ${b} es: ${resultadoMultiplicacion}`);
+console.log(`La Division de ${a} y ${b} es: ${resultadoDivision}`);
